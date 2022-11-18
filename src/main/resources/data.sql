f97b93564eddc58ad8a7338196088a1c999d3ebc
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2022-11-18 15:23:21 -0300

    adicionando spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$6W3BvZ3NyO7iC3MA1hjazupBLjLQ0J2n3SmFE3YMKYBqrvGOflIL6');
+
 INSERT INTO CATEGORIA(titulo, cor) VALUES ('Livre', 'Rosa');
 INSERT INTO CATEGORIA(titulo, cor) VALUES ('Programação', 'Verde-escuro');
 INSERT INTO CATEGORIA(titulo, cor) VALUES ('Front-end', 'Azul');
